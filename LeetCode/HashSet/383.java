25277d59b12c2186a803626dbe035eb34742ca05
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-06-09 00:57:21 +0530

    Implement canConstruct method for ransom note problem

---
+++ LeetCode/HashSet/383.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> mp= new HashMap<>();
+
+        for(int i=0; i<magazine.length(); i++){
+            char c= magazine.charAt(i);
+            if(mp.containsKey(c)){
+                mp.put(c, mp.get(c)+1);
+            }else{
+                mp.put(c, 1);
+            }
+        }
+
+         for(int i=0; i<ransomNote.length(); i++){
+            char c= ransomNote.charAt(i);
+            if(!mp.containsKey(c) || mp.get(c)== 0){
+                return false;
+            }
+
+            mp.put(c, mp.get(c)-1);
+        }
+
+        return true;
+    }
+}
