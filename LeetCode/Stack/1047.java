b9e80e49767b52db6eb41fed9e738a5d5cc31084
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-06-23 15:32:21 +0530

    Solve remove-all-adjacent-duplicates-in-string

---
+++ LeetCode/Stack/1047.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!st.empty() && st.peek() == ch) {
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+            for (char c : st) {
+                sb.append(c);
+            }
+
+        return sb.toString();
+
+    }
+}
