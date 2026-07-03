712091de4254af177191a506fd5aab4fb44866d2
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-07-03 15:18:57 +0530

    Refactor findKthLargest to use a different priority queue

--- LeetCode/Heap/215.java
+++ LeetCode/Heap/215.java
@@ -1,13 +1,32 @@
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> q = new PriorityQueue<>();
+
+//         for(int num: nums){
+//             q.offer(num);
+//             if(q.size()> k){
+//                 q.poll();
+//             }
+//         }
+//         return q.peek();
+//     }
+// }
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int num: nums){
-            q.offer(num);
-            if(q.size()> k){
-                q.poll();
+        for(int i=0; i<k; i++){
+            pq.add(nums[i]);
+        }
+        for(int i=k; i<nums.length; i++){
+            if(pq.peek()< nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
             }
         }
-        return q.peek();
+
+        return pq.peek();
     }
 }
