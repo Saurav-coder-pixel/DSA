576ff16dba83ae8ae4a219936147d038bf4ab132
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-09-13 23:48:30 +0530

    Solve No. of Islands

---
+++ LeetCode/Graphs/200.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n= grid.length;
+        int m= grid[0].length;
+        int res= 0;
+        int i, j;
+        boolean[][] vis= new boolean[n][m];
+
+        for(i=0; i<n; i++){
+            for(j=0; j<m; j++){
+                if(grid[i][j]== '1' && vis[i][j]== false){
+                    dfs(grid, n, m, i, j, vis);
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private boolean valid(int i, int j, int n, int m){
+        if(i<0 || i>=n || j<0 || j>=m){
+            return false;
+        }
+        return true;
+    }
+
+    int[] x= {-1,1,0,0};
+    int[] y= {0,0,-1,1};
+
+    private void dfs(char[][] grid, int n, int m, int i, int j, boolean[][] vis){
+        vis[i][j]= true;
+
+        for(int k=0; k<4; k++){
+            int row= i+ x[k];
+            int col= j+ y[k];
+
+            if(valid(row, col, n, m) && grid[row][col]== '1' && vis[row][col]== false){
+                dfs(grid, n, m, row, col, vis);
+            }
+        }
+    }
+}
